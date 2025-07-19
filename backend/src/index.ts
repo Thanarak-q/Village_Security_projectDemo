7348e68c24126b343bd7cb0c1c32d2868605fb07
ntchz <Nonny.1122@hotmail.com>
2025-07-19 14:50:10 +0700

    add caddy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,40 +1,42 @@
 import { Elysia } from "elysia";
-import db from "./db/drizzle"; // Import your db
-import { usersTable, User } from "./db/schema"; // Import table and type
-import { cors } from '@elysiajs/cors';
+import db from "./db/drizzle";
+import { usersTable } from "./db/schema";
+import { cors } from "@elysiajs/cors";
+
 const app = new Elysia();
+
+app.use(cors()); // ใช้ก่อน listen
 
 app.get("/", () => "Hello Elysia!");
 
-app.get("/users", async () => {
-  const allUsers = await db.select().from(usersTable);
+// กลุ่มเส้นทางที่เริ่มด้วย /api
+app.group("/api", (app) =>
+  app
+    .get("/", () => "API Root") // /api
+    .get("/users", async () => {
+      const allUsers = await db.select().from(usersTable);
+      return allUsers;
+    })
+    .post("/users", async ({ body }) => {
+      const { name, age, email } = body as {
+        name: string;
+        age: number;
+        email: string;
+      };
 
-  return allUsers;
-});
+      if (!name || !age || !email) {
+        return { error: "Missing fields!" };
+      }
 
-app.post("/users", async ({ body }) => {
-  const { name, age, email } = body as {
-    name: string;
-    age: number;
-    email: string;
-  };
+      const [newUser] = await db
+        .insert(usersTable)
+        .values({ name, age, email })
+        .returning();
 
-  if (!name || !age || !email) {
-    return { error: "Missing fields!" };
-  }
+      return newUser;
+    })
+);
 
-  const [newUser] = await db
-
-    .insert(usersTable)
-
-    .values({ name, age, email })
-
-    .returning();
-
-  return newUser;
-});
-
-app.use(cors()); // เพิ่มก่อน .listen()
 app.listen(3001, () => {
   console.log("🦊 Elysia is running at http://localhost:3001");
 });
