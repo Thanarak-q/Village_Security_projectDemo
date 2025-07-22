6fb49bcb12259f6e6fb9b3741b550df8a7d6b9f6
thanarak-q <thanarak_ka@cmu.ac.th>
2025-07-22 14:20:08 +0700

    feat: add frontend dashboard -sidebar

---
+++ frontend/src/app/dashboard/app-sidebar.tsx
@@ -0,0 +1,63 @@
+import { Calendar, Home, Inbox, Search, Settings, BookUser, Building } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import Link from "next/link"
+
+
+const items = [
+  {
+    title: "หน้าหลัก",
+    url: "/dashboard",
+    icon: Home,
+  },
+  {
+    title: "จัดการบัญชีผู้ใช้",
+    url: "/dashboard/user_manage",
+    icon: BookUser,
+  },
+  {
+    title: "จัดการบ้าน",
+    url: "/dashboard/house_manage",
+    icon: Building,
+  },
+  {
+    title: "Settings",
+    url: "/dashboard/setting_manage",
+    icon: Settings,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
