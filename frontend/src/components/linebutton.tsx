95b00768deafe8098637874dd8eaaf7e948ffd5b
thanarak-q <thanarak_ka@cmu.ac.th>
2025-09-10 13:36:50 +0700

    chore: remove outdated documentation and update API integration for chart and statistics components

--- frontend/src/components/linebutton.tsx
+++ frontend/src/components/linebutton.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const LINE_CHANNEL_ID = '2007847995';
-const REDIRECT_URI = encodeURIComponent('http://localhost/api/callback');
+const REDIRECT_URI = encodeURIComponent('http://167.71.218.163/api/callback');
 const STATE = Math.random().toString(36).substring(2);
 
 const lineLoginUrl = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${LINE_CHANNEL_ID}&redirect_uri=${REDIRECT_URI}&state=${STATE}&scope=profile%20openid`;
